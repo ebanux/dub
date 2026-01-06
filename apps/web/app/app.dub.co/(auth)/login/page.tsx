9024b3ceee6e48b072d3d7eeb9843e7256ef991b
yoandrypa <yoandrypa@gmail.com>
2026-01-06 11:03:22 -0500

    refactor: rename LoginPage to LogInPage and streamline login handling logic

--- apps/web/app/app.dub.co/(auth)/login/page.tsx
+++ apps/web/app/app.dub.co/(auth)/login/page.tsx
@@ -4,25 +4,23 @@ import { fetchAuthSessionToken, redirectToSignIn } from "@/lib/qrlynk";
 import { signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
 
-export default function LoginPage() {
+export default function LogInPage() {
   const { data: session, status } = useSession();
 
-  const loginHandle = () => {
-    fetchAuthSessionToken(true)
-      .then((token) => {
-        if (!token) redirectToSignIn(window.location.href);
-        signIn("qrlynk-auth", {
-          callbackUrl: window.location.href,
-          token: token,
-        });
-      })
-      .catch((err) => {
-        console.log("Error fetching auth session token:", err);
-      });
-  };
-
   useEffect(() => {
-    if (status !== "loading" && !session) loginHandle();
+    if (status !== "loading" && !session) {
+      fetchAuthSessionToken(true)
+        .then((token) => {
+          if (!token) redirectToSignIn(window.location.href);
+          signIn("qrlynk-auth", {
+            callbackUrl: window.location.href,
+            token: token,
+          });
+        })
+        .catch((err) => {
+          console.log("Error fetching auth session token:", err);
+        });
+    }
   }, [status, session]);
 
   return (
